b44a704bad47e2424f7306f0c1c40e770c6640a7
Klaris1234 <S10257959@connect.np.edu.sg>
2024-01-23 01:00:55 +0800

    homepage first implement

---
+++ script.js
@@ -0,0 +1,7 @@
+//map
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2xhcmlzIiwiYSI6ImNscmoyMWpocDBmZmgya3N6cWt3d3NtM2wifQ.wFSXb4hFR0fpbXiRejoPQg';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 2 // starting zoom
+    });
